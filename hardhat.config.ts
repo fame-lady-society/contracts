e817ff685cd5517d94fb1174247bd52fd44668b1
0xflick <github@0xflick.xyz>
2023-04-05 11:40:50 -0500

    add gas reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,12 @@
 import "@nomiclabs/hardhat-ethers";
+import "hardhat-gas-reporter";
+import "dotenv/config";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 export default {
   solidity: "0.8.18",
+  gasReporter: {
+    currency: "USD",
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
 };
